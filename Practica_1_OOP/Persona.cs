2c142db42418d900f0a9faec4eb56c3107440e3b
NoelDevs17 <noeldrum17@hotmail.com>
2024-04-19 18:04:25 -0400

    Add project files.

---
+++ Practica_1_OOP/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1_OOP
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int  Edad { get; set; }
+        public string  Ciudad { get; set; }
+        public Persona(string nombre, int edad, string ciudad)
+        {
+
+            Nombre = nombre;
+            Edad = edad;
+            Ciudad = ciudad;
+
+        }
+
+        Persona persona1 = new Persona("Genesis Santiago", 24, "San pedro de macoris");
+
+        public void ShowInfoPerson() { 
+            Console.WriteLine("Hola" + persona1.Nombre + "Tu Edad es: " + Edad + "y Perteneces a " + Ciudad);
+        }
+    }
+}
---
+++ Practica_1_OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Practica_1_OOP;
+
+Console.WriteLine("Hello, World!");
+
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona("Genesis Santiago", 24, "San pedro de macoris");
+
+            persona.ShowInfoPerson();
+    }
