168fdf98f446397a87a3f5e2150f4d70d8a6dfa6
NoelDevs17 <noeldrum17@hotmail.com>
2024-04-19 18:42:16 -0400

    -Avance del primer mandato de la tarea

--- Practica_1_OOP/Program.cs
+++ Practica_1_OOP/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Practica_1_OOP;
-
-Console.WriteLine("Hello, World!");
-
+    
+class Program { 
     static void Main(string[] args)
     {
-        Persona persona = new Persona("Genesis Santiago", 24, "San pedro de macoris");
 
-            persona.ShowInfoPerson();
+
+
+        Console.WriteLine("Ingrese su fecha de nacimiento (dd/MM/yyyy): ");
+        string fechaNacimientoString = Console.ReadLine();
+        Console.WriteLine("Ingrese su Nombre Completo: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese el nombre de la ciudad donde vive: ");
+        string ciudad = Console.ReadLine();
+        DateTime fechaNacimiento = DateTime.ParseExact(fechaNacimientoString, "dd/MM/yyyy", null);
+
+        Persona persona1 = new Persona(nombre, 22, ciudad, fechaNacimiento);
+
+        //métodos de la clase "Persona"
+        persona1.MostrarInformacion();
+
+        int edadXAnos = persona1.CalcularEdadXAnos();
+        Console.WriteLine("Edad en años: " + edadXAnos);
+
+        // objeto de la clase "Persona"
+        
     }
+
+}
